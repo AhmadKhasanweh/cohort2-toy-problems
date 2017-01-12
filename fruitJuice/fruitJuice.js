ecad2ec1d280a77d920afe7bd70fdada01239066
AhmadKhasawneh <Ahmad.Khasawneh@hotmail.com>
2017-01-12 09:23:42 +0200

    test

--- fruitJuice/fruitJuice.js
+++ fruitJuice/fruitJuice.js
@@ -19,7 +19,7 @@ of banana juice is only 0.25 (300 units apple juice + 100 units banana juice)
 
 Complete the functions in order to provide this functionality.
 */
-
+ 
 /*
 These are some tests:
 var jar = new Jar();
