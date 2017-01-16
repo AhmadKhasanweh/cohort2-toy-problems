e2bcb2516397f3904b05b19e23b7a5979fbf0cfd
AhmadKhasawneh <Ahmad.Khasawneh@hotmail.com>
2017-01-16 09:30:41 +0200

    stack

---
+++ queueStack/queueStack.js
@@ -0,0 +1,56 @@
+/**
+  * Write a stack using your preferred instantiation pattern. Once you're done,
+  * implement a queue using two stacks.
+  */
+
+ /**
+   * Stack Class
+   */
+ var Stack = function() {
+  this.stack=[];
+
+   // add an item to the top of the stack
+   this.push = function(value){
+   	let temp = [value];
+    this.stack = temp.concat(this.stack);
+   };
+
+   // remove an item from the top of the stack
+   this.pop = function(){
+   	let temp = this.stack[0];
+   	this.stack = this.stack.slice(1);
+    return temp;
+   };
+
+   // return the number of items in the stack
+   this.size = function(){
+   	return this.stack.length
+   };
+ };
+
+ /**
+   * Queue Class
+   */
+ var Queue = function() {
+   // Use two `stack` instances to implement your `queue` Class
+   var inbox = new Stack();
+   var outbox = new Stack();
+
+   // called to add an item to the `queue`
+   this.enqueue = function(value){
+     inbox.push( value );
+   };
+
+   // called to remove an item from the `queue`
+   this.dequeue = function(){
+   	  while( inbox.pop() !== undefined ) {
+   		outbox.push( inbox.pop() );
+   	  }
+    return outbox.pop();
+   };
+
+   // should return the number of items in the queue
+   this.size = function(){
+   	return inbox.size()
+   }
+ }
